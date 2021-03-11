844fff94145dffff878c43369607fb6f793d783a
yahav sasporta <yahav2011@gmail.com>
2021-03-11 03:43:44 +0200

    fixed few bugs at heroku deployment v3

--- server/server.js
+++ server/server.js
@@ -13,10 +13,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 if(process.env.NODE_ENV === ' production') {
-    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.use(express.static(path.join(__dirname, '../client/build')));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+        res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
     });
 };
 
